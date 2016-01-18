8677bcbd8c45c87a6186df45b9ca2b289c51624f
lixi <xianglei.li@eurecom.fr>
2016-01-18 11:14:01 +0100

    INIT

---
+++ README.md
@@ -0,0 +1,3 @@
+# PopupCustomDialog
+## Description
+When you click the button **POPUP DIALOG**, a custom dialog which containing a title, a list of images (which are letters) and two buttons will slides in from the top. When you dismiss this custom dialog, it will slide out towards the top.
---
+++ app/src/main/java/fr/eurecom/lixi/popupcustomdialog/CustomDialog.java
@@ -0,0 +1,184 @@
+package fr.eurecom.lixi.popupcustomdialog;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+/**
+ * Created by lxllx on 2016/1/18.
+ */
+public class CustomDialog extends Dialog {
+
+    public CustomDialog(Context context) {
+        super(context);
+    }
+
+    public CustomDialog(Context context, int theme) {
+        super(context, theme);
+    }
+
+    public static class Builder {
+        private Context context;
+        private String title;
+        private int[] image = new int[7];
+        private String positiveButtonText;
+        private String negativeButtonText;
+        private View contentView;
+        private DialogInterface.OnClickListener positiveButtonClickListener;
+        private DialogInterface.OnClickListener negativeButtonClickListener;
+
+        public Builder(Context context) {
+            this.context = context;
+
+        }
+
+        public Builder setImages(int img01, int img02, int img03, int img04, int img05, int img06, int img07) {
+            this.image[0] = img01;
+            this.image[1] = img02;
+            this.image[2] = img03;
+            this.image[3] = img04;
+            this.image[4] = img05;
+            this.image[5] = img06;
+            this.image[6] = img07;
+            return this;
+        }
+
+        /**
+         * Set the custome_dialog title from resource
+         *
+         * @param title
+         * @return
+         */
+        public Builder setTitle(int title) {
+            this.title = (String) context.getText(title);
+            return this;
+        }
+
+        /**
+         * Set the custome_dialog title from String
+         *
+         * @param title
+         * @return
+         */
+
+        public Builder setTitle(String title) {
+            this.title = title;
+            return this;
+        }
+
+        public Builder setContentView(View v) {
+            this.contentView = v;
+            return this;
+        }
+
+        /**
+         * Set the positive button resource and it's listener
+         *
+         * @param positiveButtonText
+         * @return
+         */
+        public Builder setPositiveButton(int positiveButtonText,
+                                         DialogInterface.OnClickListener listener) {
+            this.positiveButtonText = (String) context
+                    .getText(positiveButtonText);
+            this.positiveButtonClickListener = listener;
+            return this;
+        }
+
+        public Builder setPositiveButton(String positiveButtonText,
+                                         DialogInterface.OnClickListener listener) {
+            this.positiveButtonText = positiveButtonText;
+            this.positiveButtonClickListener = listener;
+            return this;
+        }
+
+        public Builder setNegativeButton(int negativeButtonText,
+                                         DialogInterface.OnClickListener listener) {
+            this.negativeButtonText = (String) context
+                    .getText(negativeButtonText);
+            this.negativeButtonClickListener = listener;
+            return this;
+        }
+
+        public Builder setNegativeButton(String negativeButtonText,
+                                         DialogInterface.OnClickListener listener) {
+            this.negativeButtonText = negativeButtonText;
+            this.negativeButtonClickListener = listener;
+            return this;
+        }
+
+        public CustomDialog create() {
+            LayoutInflater inflater = (LayoutInflater) context
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            // instantiate the dialog with the custom Theme
+            final CustomDialog dialog = new CustomDialog(context,R.style.custome_dialog);
+            View layout = inflater.inflate(R.layout.custom_dialog, null);
+            dialog.addContentView(layout, new LayoutParams(
+                    LayoutParams.FILL_PARENT, LayoutParams.WRAP_CONTENT));
+            // set the dialog title
+            ((TextView) layout.findViewById(R.id.title)).setText(title);
+            // set the confirm button
+            if (positiveButtonText != null) {
+                ((Button) layout.findViewById(R.id.positiveButton))
+                        .setText(positiveButtonText);
+                if (positiveButtonClickListener != null) {
+                    ((Button) layout.findViewById(R.id.positiveButton))
+                            .setOnClickListener(new View.OnClickListener() {
+                                public void onClick(View v) {
+                                    positiveButtonClickListener.onClick(dialog,
+                                            DialogInterface.BUTTON_POSITIVE);
+                                }
+                            });
+                }
+            } else {
+                // if no confirm button just set the visibility to GONE
+                layout.findViewById(R.id.positiveButton).setVisibility(
+                        View.GONE);
+            }
+            // set the cancel button
+            if (negativeButtonText != null) {
+                ((Button) layout.findViewById(R.id.negativeButton))
+                        .setText(negativeButtonText);
+                if (negativeButtonClickListener != null) {
+                    ((Button) layout.findViewById(R.id.negativeButton))
+                            .setOnClickListener(new View.OnClickListener() {
+                                public void onClick(View v) {
+                                    negativeButtonClickListener.onClick(dialog,
+                                            DialogInterface.BUTTON_NEGATIVE);
+                                }
+                            });
+                }
+            } else {
+                // if no confirm button just set the visibility to GONE
+                layout.findViewById(R.id.negativeButton).setVisibility(
+                        View.GONE);
+            }
+            // set the content message
+            if (image != null) {
+                ((ImageView) layout.findViewById(R.id.image01)).setImageResource(image[0]);
+                ((ImageView) layout.findViewById(R.id.image02)).setImageResource(image[1]);
+                ((ImageView) layout.findViewById(R.id.image03)).setImageResource(image[2]);
+                ((ImageView) layout.findViewById(R.id.image04)).setImageResource(image[3]);
+                ((ImageView) layout.findViewById(R.id.image05)).setImageResource(image[4]);
+                ((ImageView) layout.findViewById(R.id.image06)).setImageResource(image[5]);
+                ((ImageView) layout.findViewById(R.id.image07)).setImageResource(image[6]);
+            } else if (contentView != null) {
+                // if no message set
+                // add the contentView to the dialog body
+                ((LinearLayout) layout.findViewById(R.id.content))
+                        .removeAllViews();
+                ((LinearLayout) layout.findViewById(R.id.content))
+                        .addView(contentView, new LayoutParams(LayoutParams.FILL_PARENT,LayoutParams.FILL_PARENT));
+            }
+            dialog.setContentView(layout);
+            return dialog;
+        }
+    }
+}
